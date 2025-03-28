c035d6aa7540fc9c58e8573979891396966c115f
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-03-28 16:09:34 +0100

    Add CORS dependency and implement posts fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
-const api_endpoint = 'http://localhost:3003/api/v1/posts'
+const base_api_url = 'http://localhost:3003'
+const posts_endpoint = '/api/v1/posts'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetchData(base_api_url + posts_endpoint)
+  }, [])
+
+  function fetchData(url) {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setPosts(data)
+      })
+  }
 
   return (
     <>
+      <main>
+        <section className='posts'>
+          <div className='container'>
+            <div className='row'>
 
+              {
+                posts.map((post, index) => (
+                  <div className='col' key={index}>
+                    <div className='card'>
+                      <img className='img-card-body' src={base_api_url + post.image} alt="" />
+                      <div className='card-body'>
+                        {post.title}
+                      </div>
+                    </div>
+                  </div>
+                ))
+              }
+
+            </div>
+          </div>
+        </section>
+      </main >
     </>
   )
 }
