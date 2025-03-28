d5db5f540f3b2918b5faedcc1d232120020e0786
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-03-28 15:13:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const api_endpoint = 'http://localhost:3003/api/v1/posts'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
